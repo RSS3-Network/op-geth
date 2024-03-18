ab3c326c35c507168aabf8d2b1afbf26e1e837a1
Albert <albert@rss3.io>
2024-03-18 20:53:38 +0800

    feat: use info log

--- miner/worker.go
+++ miner/worker.go
@@ -907,7 +907,7 @@ func (w *worker) commitTransactions(env *environment, txs *transactionsByPriceAn
 		default:
 			// Transaction is regarded as invalid, drop all consecutive transactions from
 			// the same sender because of `nonce-too-high` clause.
-			log.Debug("Transaction failed, account skipped", "hash", ltx.Hash, "err", err)
+			log.Info("Transaction failed, account skipped", "hash", ltx.Hash, "err", err)
 			txs.Pop()
 		}
 	}
