83969f1f50cfe7757b5b3f9707d55a33af19febe
iavl <z6521256@gmail.com>
2024-07-11 14:09:44 +0800

    feat: update isDeveloperVerificationEnabled

--- core/evm.go
+++ core/evm.go
@@ -172,9 +172,9 @@ func CanCreate(db vm.StateDB, addr common.Address, height *big.Int) bool {
 func isDeveloperVerificationEnabled(db vm.StateDB) bool {
 	compactValue := db.GetState(params.DeveloperListContractAddr, common.Hash{})
 	// Layout of slot 0:
-	// [0][1 - 31]
-	// [enabled][zero bytes]
-	enabledByte := compactValue.Bytes()[0]
+	// [0   -   30][   31  ]
+	// [zero bytes][enabled]
+	enabledByte := compactValue.Bytes()[common.HashLength-1]
 	return enabledByte == 0x01
 }
 
